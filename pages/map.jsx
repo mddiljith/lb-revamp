22d4402e22a0289f9e2fc281dbd0b75e1fa86f74
mddiljith@gmail.com <mddiljith@gmail.com>
2023-11-04 15:12:52 +0530

    Removing unwanted code from map.jsx

--- pages/map.jsx
+++ pages/map.jsx
@@ -1,8 +1,7 @@
-import MapboxMap from "@/Components/Map_Mapbox/MapboxMap";
 import React from "react";
 
 function Map() {
-  return <MapboxMap />;
+  // return <MapboxMap />;
 }
 
 export default Map;
