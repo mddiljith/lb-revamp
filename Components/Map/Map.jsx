6be45e67b5894eceac26845d26ce1d06c5b03689
mddiljith@gmail.com <mddiljith@gmail.com>
2023-11-21 22:46:59 +0530

    Trip view page logic

--- Components/Map/Map.jsx
+++ Components/Map/Map.jsx
@@ -37,16 +37,6 @@ function Map({ position, path }) {
   let mapplsPluginObject = new mappls_plugin();
   let markerObject;
   let polylineObject;
-  // let direction_option = {
-  //   Resource: "route_eta",
-  //   annotations: "nodes,congestion",
-  //   map: mapObject,
-  //   start: "28.545,77.545",
-  //   end: {
-  //     label: "India Gate, Delhi",
-  //     geoposition: `11.258,75.795`,
-  //   },
-  // };
   let points;
   const getPoints = (data) => {
     console.log(data);
@@ -63,47 +53,11 @@ function Map({ position, path }) {
         map: mapObject,
         position: geoData,
         clusters: true,
-        // fitbounds: true,
-        // fitboundOptions: { padding: 120, duration: 1000 },
       });
-
-    //   polylineObject = mapplsClassObject.Polyline({
-    //      map: mapObject,
-    //      path: [position],
-	//      strokeColor: "#1e40af",
-    //      fitbounds: true, // make map fit to polyline cordinates
-    //      fitboundOptions: {
-    //        padding: 120,
-    //        duration: 1000,
-    //        maxZoom: 19,
-    //      }, 
-    //      visibility: true//fitbound options
-    //    });
 
        polylineObject = mapplsClassObject.Polyline(
         {
             map: mapObject,
-            // path:[
-            //   {lat: 12.845155, lng: 77.660212},
-            //   {lat: 12.845125, lng: 77.660093},
-            //   {lat: 12.845093, lng: 77.659994},
-            //   {lat: 12.845121, lng: 77.659282},
-            //   {lat: 12.845356, lng: 77.659152},
-            //   {lat: 12.846032, lng: 77.658932},
-            //   {lat: 12.846527, lng: 77.658735},
-            //   {lat: 12.847387, lng: 77.65833},
-            //   {lat: 12.847907, lng: 77.658056},
-            //   {lat: 12.848428, lng: 77.657905},
-            //   {lat: 12.848597, lng: 77.657888},
-            //   {lat: 12.848839, lng: 77.657863},
-            //   {lat: 12.849152, lng: 77.657825},
-            //   {lat: 12.849333, lng: 77.6578},
-            //   {lat: 12.849786, lng: 77.657747},
-            //   {lat: 12.850065, lng: 77.657698},
-            //   {lat: 12.850304, lng: 77.657651},
-            //   {lat: 12.850603, lng: 77.657595},
-            //   {lat: 12.850879, lng: 77.657534}
-            // ]
             path: path
         }
     );
--- pages/trip/[id].jsx
+++ pages/trip/[id].jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import DriverLayout from '@/Components/Driver/DriverLayout';
 
 const TripMain = () => {
-  return <div>
-    Trip data
-  </div>;
+  return (
+    <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3 ...">
+      <div class="col-span-2">
+        Map area
+      </div>
+      <div class="col-span-2">
+        Some other section
+      </div>
+      <div>Trip timeline</div>
+      <div>Yer another section</div>
+    </div>
+  );
 }
 
 export default TripMain;
