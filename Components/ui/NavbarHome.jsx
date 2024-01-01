9f04513ed0a4a051631bc5d4bcd7c23618d369cc
Ebinsha <ebinsha.thaj@gmail.com>
2024-01-01 15:32:42 +0530

    mobilenavbar fix

--- Components/ui/NavbarHome.jsx
+++ Components/ui/NavbarHome.jsx
@@ -1,7 +1,16 @@
-import { Avatar, Button, Navbar, Typography } from "@material-tailwind/react";
+import {
+  Avatar,
+  Button,
+  Collapse,
+  IconButton,
+  MobileNav,
+  Navbar,
+  Typography,
+} from "@material-tailwind/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
+import { MdMenu, MdClose } from "react-icons/md";
 
 const navList = (
   <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
@@ -43,6 +52,7 @@ const navList = (
 
 function NavbarHome() {
   const router = useRouter();
+  const [openNav, setOpenNav] = useState(true);
   return (
     <Navbar className="mx-auto max-w-screen px-4 py-2 lg:px-8 lg:py-4 sticky top-0 z-10 h-max rounded-none">
       <div className="flex justify-between">
@@ -58,14 +68,49 @@ function NavbarHome() {
           <Button
             variant="gradient"
             onClick={() => router.push("/auth/signUp")}
+            className="hidden lg:inline-block"
           >
             Sign Up
           </Button>
-          <Button variant="outlined" onClick={() => router.push("/auth/login")}>
+          <Button
+            variant="outlined"
+            onClick={() => router.push("/auth/login")}
+            className="hidden lg:inline-block"
+          >
             Sign In
           </Button>
         </div>
+        <IconButton
+          variant="text"
+          className="ml-auto hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          <MdMenu size={"1.5rem"} />
+        </IconButton>
       </div>
+
+      <Collapse open={openNav}>
+        {navList}
+        <div className="flex items-center gap-x-1">
+          <Button
+            fullWidth
+            variant="outlined"
+            size="sm"
+            onClick={() => router.push("/auth/login")}
+          >
+            <span>Sign In</span>
+          </Button>
+          <Button
+            fullWidth
+            variant="gradient"
+            size="sm"
+            onClick={() => router.push("/auth/signUp")}
+          >
+            <span>Sign Up</span>
+          </Button>
+        </div>
+      </Collapse>
     </Navbar>
   );
 }
